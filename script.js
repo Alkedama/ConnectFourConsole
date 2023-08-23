43e96ce20dcdd35c097708fa01be85806f5bbc98
King-Windows <krsazon@gmail.com>
2023-08-22 23:04:29 -0700

    2D arrays #2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
 ** The Gameboard represents the state of the board
-** Each equare holds a Cell (defined later)
+** Each square holds a Cell (defined later)
 ** and we expose a dropToken method to be able to add Cells to squares
 */
 
